630ad8d0662ea74c91588c70566f3e56bcbbb3ed
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-15 16:23:11 -0700

    ffjfjk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
-main-entry point
-Discription:A c print string by puts function
-return : alawys 0 (success)
+ * main-entry point
+ * Discription:A-c print string by puts function
+ * Return : alawys 0 (success)
 */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
