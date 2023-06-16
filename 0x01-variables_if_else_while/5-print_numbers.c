5762bd55b9b52d64b1d0cebc55fa73dced4fcb1c
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-16 15:52:59 -0700

    hjehw

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * discription : Print all the letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+	printf("%d", i);
+	putchar('\n');
+	return (0);
+}
