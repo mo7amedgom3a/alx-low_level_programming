4a55aeaa97dac2f822c3a488dbd015bc83da9ea9
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-16 16:11:24 -0700

    dijhhf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry point
+ *
+ * discription : Print all the letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 48 ; i <= 57 ; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
