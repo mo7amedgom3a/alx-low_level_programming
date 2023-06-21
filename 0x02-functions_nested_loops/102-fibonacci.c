e99f678e540d50206f0bfa7fa44f9a7a88dd0bc7
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-21 09:33:35 -0700

    rehkb

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	int i, a = 1, b = 2, c;
+	int a = 1, b = 2, c;
 
 	printf("%d, %d, ", a, b);
 
-	for (int i = 3; i <= 50; i++)
+	int i;
+
+	for (i = 3; i <= 50; i++)
 	{
 		c = a + b;
 		if (i != 50)
