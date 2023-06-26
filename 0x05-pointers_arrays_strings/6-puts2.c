ed5bff03641693b437222f02fb9789fd5bbe521e
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-26 15:20:21 -0700

    test

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints part of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ **/
+void puts2(char *str)
+{
+	int l = strln(str), i;
+
+	for (i = 0; i < l; i += 2)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
