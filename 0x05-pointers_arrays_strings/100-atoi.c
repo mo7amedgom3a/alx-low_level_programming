1606e541a287015642f86b7264ad9c74c0460e88
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-26 17:38:52 -0700

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 	unsigned int num = 0, sig = 1;
 
 	do {
-		if (sig == '-')
+		if (*s == '-')
 			sig *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
