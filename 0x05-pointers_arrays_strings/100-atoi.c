428a7660a5a0fa379e93ced81989c78f44283ab2
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-06-26 17:16:04 -0700

    test

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,14 @@ int _atoi(char *s)
 {
 	int i, num, sig = 1;
 
-	if (*(s + i) == '-')
+	if (*s == '-')
 	{
 		sig = -1;
 		i++;
 	}
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		num = num * 10 + (*(s + i) - '0');
+		num = num * 10 + (*s - '0');
 		i++;
 	}
 	return (num * sig);
