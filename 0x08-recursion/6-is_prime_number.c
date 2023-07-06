f90ebada8ab68b623f3f9a9d75c95169aa6a0bae
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-05 18:35:47 -0700

    test

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,5 @@
-#include "main.h"
-#include <math.h>
 /**
- * is_prime_recursive - Checks to see if number is prime
+ * check - Checks to see if number is prime
  *
  * @i: Factor check
  *
@@ -10,17 +8,17 @@
  * Return: 1 if prime, 0 if not
  *
  */
-int is_prime_recursive(int n, int i)
-{
-	if (n <= 2)
-		return (n == 0);
-	if (i > n / 2)
-		return (1);
-	if (n % i == 0)
-		return (0);
 
-	return (n, i + 1);
+int check(int n, int i)
+{
+	if (n < 2 || n % i == 0)
+		return (0);
+	else if (i > n / 2)
+		return (1);
+	else
+		return (check(n, i + 1));
 }
+
 /**
  * is_prime_number - States if number is prime
  *
@@ -31,5 +29,7 @@ int is_prime_recursive(int n, int i)
 
 int is_prime_number(int n)
 {
-	return (is_prime_recursive(n, 2));
+	if (n == 2)
+		return (1);
+	return (check(2, n));
 }
