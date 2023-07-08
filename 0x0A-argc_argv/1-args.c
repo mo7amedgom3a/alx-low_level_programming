558ce70cca6a0a5e5767fd4392c8718af0179a75
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-07 19:41:51 -0700

    test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Print number of argument
+ *
+ * @argc: Length of argv
+ *
+ * @argv: Arrays of strings of the program arguments
+ *
+ * Return: 0, Success
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc);
+	return (0);
+}
