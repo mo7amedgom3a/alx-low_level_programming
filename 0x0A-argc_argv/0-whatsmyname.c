4e6a857f8bc5a8d9faba1ecf26e0069fa848ee12
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-07 19:38:42 -0700

    test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints its name
+ *
+ * @argc: Length of argv
+ *
+ * @argv: Arrays of strings of the program arguments
+ *
+ * Return: 0, Success
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
