dbfeba721fd2c3200646f2dd2efa49d5ce5bad13
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-07 20:05:10 -0700

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,13 +6,13 @@
  *
  * @argv: Array of strings of the arguments of this program
  *
- * Return: 0, Success
+ * Return: 0, Success or 1 fail
  */
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	}
 	else
 	{
