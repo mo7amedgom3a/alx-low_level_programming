1b918e5da4f7a4e99ae1c2853f3ee9e2c1b089f7
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-10 14:17:48 -0700

    test

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,12 +15,12 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)calloc(width, sizeof(int *));
+	arr = (int **)calloc(height, sizeof(int *));
 
 	if (arr == NULL)
 	return (NULL);
 
-	for (i = 0; i < width; i++)
-		arr[i] = (int *)calloc(height, sizeof(int));
+	for (i = 0; i < height; i++)
+		arr[i] = (int *)calloc(width, sizeof(int));
 	return (arr);
 }
