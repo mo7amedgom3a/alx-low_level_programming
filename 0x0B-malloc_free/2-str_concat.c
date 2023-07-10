22079229db7dff541e56404b44397108846b4ad7
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-10 13:08:28 -0700

    test

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,11 @@ char *str_concat(char *s1, char *s2)
 	if ((s1 == NULL) && (s2 == NULL))
 		return (NULL);
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	size = strlen(s1) + strlen(s2) + 1;
 
