fd218a14904202d08baa27b573655d73c2d34755
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-10 08:51:46 -0700

    test

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,11 @@ char *create_array(unsigned int size, char c)
 {
 	int i;
 	char *ptr;
+
 	if (size == 0)
 		return (NULL);
 
-	ptr = (char*)malloc(size);
+	ptr = (char *)malloc(size);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
