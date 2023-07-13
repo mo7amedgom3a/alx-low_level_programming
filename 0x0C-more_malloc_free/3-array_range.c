eece42e45b388ca0734790cb2e84c2b5f0c37ff6
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-13 13:27:51 -0700

    test

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,8 +15,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	size = max - min + 1;
+	size = (max - min) + 1;
 	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+		return (NULL);
 
 	for (int i = 0; i < size; i++)
 		arr[i] = min++;
