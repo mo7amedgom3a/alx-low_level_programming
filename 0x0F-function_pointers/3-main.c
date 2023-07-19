884106ba76cb07ec950a838faec298915753b671
mohamedGomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-19 14:36:41 -0700

    test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: length of command line arguments
+ *
+ * @argv: double pointer to cli arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, int), a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	ptr = get_op_func(argv[2]);
+
+	if (!ptr)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = ptr(atoi(argv[1]);
+	b = ptr(atoi(argv[3]));
+
+	printf("%d\n", ptr(a, b));
+
+	return (0);
+}
