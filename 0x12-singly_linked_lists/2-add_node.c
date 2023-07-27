eeed19a55edfa88587763322d283aa9c8b3be6dc
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-27 11:34:45 -0700

    test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,15 +10,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_head = malloc(sizeof(list_t));
 
-	if (!head || !new_head)
+	if (new_head == NULL)
+	{
 		return (NULL);
-
-	new_node->str = strdup(str);
-	new_node->next = *head;
-	new_node->len = strlen(new_node->str);
-	*head = new_node;
-
-	return (new_node);
+	}
+	else
+	{
+		new_head->str = strdup(str);
+		new_head->len = length;
+		new_head->next = *head;
+		*head = new;
+		return (new_head);
+	}
 }
