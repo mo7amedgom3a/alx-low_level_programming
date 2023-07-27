19c081a43500a4cebb33e1a87598ebbefce75d59
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-07-27 10:54:07 -0700

    test

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "list.h"
+/**
+ * print_list - prints to standard output
+ * @h: A node in list_t
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
