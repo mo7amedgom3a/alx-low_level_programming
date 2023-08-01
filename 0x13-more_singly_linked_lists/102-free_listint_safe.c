a8dd124c92e655231d98563930d7c8612d1a739c
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-01 15:35:49 -0700

    test

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,22 +1,37 @@
 #include "lists.h"
 /**
- * free_listint_safe - this function free a list in safe mode
+ * free_listint_safe - thsi function free a list in safe mode
  * @h: the head of list
  * Description: this function free a string in a safe mode
  * section header: the header of this function is lists.h)*
  * Return: the size of the list
  */
+
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *current = *h, *next = NULL;
+	listint_t *tmp, *node;
+	size_t size;
+	int div;
 
-	while (current != NULL)
+	size = 0, node = *h;
+
+	while (node != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
-		size++;
+		div = node - node->next;
+		if (div > 0)
+		{
+			tmp = node->next;
+			free(node);
+			node = tmp;
+			size++;
+		} else
+		{
+			free(node);
+			*h = NULL;
+			size++;
+			break;
+		}
+
 	}
 
 	*h = NULL;
