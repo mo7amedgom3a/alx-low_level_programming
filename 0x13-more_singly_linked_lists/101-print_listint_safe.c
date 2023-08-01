7a922582dc5216e661c31fe7a6381f3e19d9420c
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-01 15:01:19 -0700

    test

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,62 @@
 #include "lists.h"
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *current = head, *prev = NULL;
+    size_t count = 0;
+    const listint_t *tortoise = head, *hare = head;
 
-	while (current != NULL)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
+    if (head == NULL)
+        return (0);
 
-		if (current < prev)
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
+    while (1)
+    {
+        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+        count++;
 
-		prev = current;
-		current = current->next;
-	}
+        if (hare == NULL || hare->next == NULL)
+            break;
 
-	return (count);
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if (tortoise == hare)
+        {
+            tortoise = head;
+
+            while (tortoise != hare)
+            {
+                printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+                count++;
+                tortoise = tortoise->next;
+                hare = hare->next;
+            }
+
+            printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+            count++;
+
+            hare = hare->next;
+            while (tortoise != hare)
+            {
+                printf("[%p] %d\n", (void *)hare, hare->n);
+                count++;
+                hare = hare->next;
+            }
+
+            return (count);
+        }
+    }
+
+    while (tortoise != NULL)
+    {
+        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+        count++;
+        tortoise = tortoise->next;
+    }
+
+    return (count);
 }
