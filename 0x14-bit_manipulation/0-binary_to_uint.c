ec82fb7ccd626ddb8d636ee302a6518935fec3c8
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-03 14:46:31 -0700

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int len = strlen(b);
 
 	if (b == NULL)
 		return (0);
-	for (int i = 0; i < len; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
+	while (*b)
+		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
-		num = num * 2 + (b[i] - '0');
+		num = num * 2 + (*b++ - '0');
 	}
 
 	return (num);
