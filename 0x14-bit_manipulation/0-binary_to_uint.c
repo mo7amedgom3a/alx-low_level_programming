8762f462923225e6bb4e1089cfe797f5389dc44f
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-03 14:41:43 -0700

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,46 +3,26 @@
 #include <stdio.h>
 
 /**
- * _strlen - find the length of a string
- * @s: pointer to the string to check
- * Return: void
-*/
-
-
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i])
-	i++;
-
-return (i);
-}
-
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: binary number
  *
  * Return: 0 or converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n = 0;
-	int i, len;
+	unsigned int num = 0;
+	int len = strlen(b);
 
 	if (b == NULL)
 		return (0);
-
-	len = _strlen(b);
-
-	for (i = 0; i != len; i++)
+	for (int i = 0; i < len; i++)
 	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
-		else if (b[len - i - 1] != '0')
+		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
+		num += (b[i] - '0') * (1 << (len - i - 1));
 	}
 
-	return (n);
+	return (num);
 }
