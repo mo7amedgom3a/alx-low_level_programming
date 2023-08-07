528bbfd357b3c89e5e336e8a754917a52acde3e6
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-07 16:28:12 -0700

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <unstid>
-#include <stdlib>
+#include <unistd.h>
+#include <stdlib.h>
 int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);ssize_t read_textfile(const char *filename, size_t letters);
 #endif
