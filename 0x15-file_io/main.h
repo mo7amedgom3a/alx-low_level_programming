7630e6926abd098659aaddf8a631311eb82ef790
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-08-07 16:31:15 -0700

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
-int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);ssize_t read_textfile(const char *filename, size_t letters);
+
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif
