b79175ed7c29861142eaa837dbb971ef09424158
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-04-11 08:21:52 +0200

    test

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -15,34 +15,31 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step = 0, step_size = sqrt(size);
-	listint_t *prev = list, *jump = list;
+	size_t step, step_size;
+	listint_t *node, *jump;
 
 	if (list == NULL || size == 0)
 		return (NULL);
 
-	while (jump->index + 1 < size && jump->n < value)
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		prev = jump;
-		while (jump->index < step)
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
 		{
-			jump = jump->next;
 			if (jump->index + 1 == size)
 				break;
 		}
-		step += step_size;
 		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n",
-			prev->index, jump->index);
+			node->index, jump->index);
 
-	while (prev->index < jump->index && prev->n < value)
-	{
-		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
-		prev = prev->next;
-	}
-	printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-	return (prev->n == value ? prev : NULL);
+	return (node->n == value ? node : NULL);
 }
